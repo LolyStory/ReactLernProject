e6735e9a188773e87a35ce22264d56dec7f246b4
Aleksey <lolystory777@gmail.com>
2022-12-15 20:22:28 +0300

    Add header component

---
+++ notes/src/App.js
@@ -0,0 +1,11 @@
+import { Header } from "./components/header";
+
+function App() {
+    return (
+        <div className="App">
+            <Header title={"На большом плоту"} />
+        </div>
+    );
+}
+
+export default App;
---
+++ notes/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export function Header ({title}) {
+    return (
+        <>
+            <div className="header">
+                {title}
+            </div>
+        </>
+    )
+};
+//"На маленьком плоту" 
+//компонента футера c svg(from Loza), принимающая в 
+//себя надпись и он снизу потом я делаю пулреквест на лозу
