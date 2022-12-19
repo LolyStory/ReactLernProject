6a58e17565246b0995a4641374d37edbd0ae58c4
Aleksey <lolystory777@gmail.com>
2022-12-19 14:40:52 +0300

    Delete <></>, and cleare code

--- notes/src/components/footer/Footer.jsx
+++ notes/src/components/footer/Footer.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import "./styles.css";
 
 export const Footer = ({ title }) => (
-        <>
-            <div className="footer">
-                <div className="footer title">
-                    {title}
-                </div>   
-                <div className="footer svg"></div>
-            </div>
-        </>
-    )
+    <div className="footer">
+        <div className="footer title">{title}</div>
+        <div className="footer svg"></div>
+    </div>
+);
 
-//"На маленьком плоту" 
-//компонента футера c svg(from Loza), принимающая в 
+//"На маленьком плоту"
+//компонента футера c svg(from Loza), принимающая в
 //себя надпись и он снизу потом я делаю пулреквест на лозу
--- notes/src/components/header/Header.jsx
+++ notes/src/components/header/Header.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 import "./styles.css";
 
-export const Header = ({ title }) => (
-        <>
-            <div className="header">
-                {title}
-            </div>
-        </>
-    );
+export const Header = ({ title }) => <div className="header">{title}</div>;
 
-//"На маленьком плоту" 
-//компонента футера c svg(from Loza), принимающая в 
+//"На маленьком плоту"
+//компонента футера c svg(from Loza), принимающая в
 //себя надпись и он снизу потом я делаю пулреквест на лозу
