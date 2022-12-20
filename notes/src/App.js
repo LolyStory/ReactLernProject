00e484434efb1a3b5363fb7ff620dc30f97fb81f
Aleksey <lolystory777@gmail.com>
2022-12-20 11:41:57 +0300

    Add content from Fake api. Fix position footer. Style content.

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,10 +1,12 @@
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
+import { PostsList } from "./conteiners/PostsList";
 
 function App() {
     return (
         <div className="App">
             <Header title={"На большом плоту"} />
+            <PostsList />
             <Footer title={"Мне не дали свг, поэтому тут он, да он классный"} />
         </div>
     );
